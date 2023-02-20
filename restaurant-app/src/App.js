0f0f8649b3be4a8062a2e707f1c9cdeb856cafdc
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-20 19:39:00 +0530

    Added routing to various components

--- restaurant-app/src/App.js
+++ restaurant-app/src/App.js
@@ -1,14 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Route,Link, Routes} from 'react-router-dom';
+import Home from "./components/Home";
+import RestaurantList from "./components/RestaurantList";
+import RestaurantCreate from "./components/RestaurantCreate";
+import RestaurantSearch from "./components/RestaurantSearch";
+import RestaurantDetail from "./components/RestaurantDetail";
+import RestaurantUpdate from "./components/RestaurantUpdate";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        
-      </header>
+    <Router>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/list">List</Link></li>
+        <li><Link to="/create">Create</Link></li>
+        <li><Link to="/search">Search</Link></li>
+        <li><Link to="/details">Details</Link></li>
+        <li><Link to="/update">Update</Link></li>
+      </ul>
+      <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/list" element={<RestaurantList />}></Route>
+      <Route path="/create" element={<RestaurantCreate />}></Route>
+      <Route path="/search" element={<RestaurantSearch />}></Route>
+      <Route path="/details" element={<RestaurantDetail />}></Route>
+      <Route path="/update" element={<RestaurantUpdate />}></Route>
+      </Routes>
+    </Router>
+      
     </div>
   );
 }
