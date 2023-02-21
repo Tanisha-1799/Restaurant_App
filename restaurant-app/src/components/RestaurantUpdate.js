207ea6e8c78f9e47b1ed3bacc6f1dd29b95f16b3
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-21 19:24:33 +0530

    Adding the update api to the app

--- restaurant-app/src/components/RestaurantUpdate.js
+++ restaurant-app/src/components/RestaurantUpdate.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class RestaurantUpdate extends Component {
   render() {
     return (
-      <div>RestaurantUpdate</div>
+      <div>
+        <h1>Restaurant Update component</h1>
+      </div>
     )
   }
 }
