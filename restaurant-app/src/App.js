13bd860a08430239839bba493b30b905ded2eb71
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-21 18:53:42 +0530

    Adding bootstrap

--- restaurant-app/src/App.js
+++ restaurant-app/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {BrowserRouter as Router,Route,Link, Routes} from 'react-router-dom';
+import {Navbar,Nav,Container} from "react-bootstrap";
 import Home from "./components/Home";
 import RestaurantList from "./components/RestaurantList";
 import RestaurantCreate from "./components/RestaurantCreate";
@@ -11,14 +12,22 @@ function App() {
   return (
     <div className="App">
     <Router>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/list">List</Link></li>
-        <li><Link to="/create">Create</Link></li>
-        <li><Link to="/search">Search</Link></li>
-        <li><Link to="/details">Details</Link></li>
-        <li><Link to="/update">Update</Link></li>
-      </ul>
+    <Navbar bg="light" expand="lg">
+      
+        <Navbar.Brand href="#home" className='brand'>Resto</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home"><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link href="#link"><Link to="/list">List</Link></Nav.Link>
+            <Nav.Link href="#link"><Link to="/create">Create</Link></Nav.Link>
+            <Nav.Link href="#link"><Link to="/search">Search</Link></Nav.Link>
+            <Nav.Link href="#link"><Link to="/update">Update</Link></Nav.Link>            
+          </Nav>
+        </Navbar.Collapse>
+      
+    </Navbar>
+    
       <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/list" element={<RestaurantList />}></Route>
--- restaurant-app/src/components/RestaurantList.js
+++ restaurant-app/src/components/RestaurantList.js
@@ -1,42 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Table } from "react-bootstrap";
 
 export default class RestaurantList extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      list : null,
-    }
+    this.state = {
+      list: null,
+    };
   }
-  componentDidMount(){
-    fetch("http://localhost:3000/restaurant").then((response)=>{
-      response.json().then((result)=>{
+  componentDidMount() {
+    fetch("http://localhost:3000/restaurant").then((response) => {
+      response.json().then((result) => {
         //console.warn(result);
-        this.setState({list:result});
-      })
-    })
+        this.setState({ list: result });
+      });
+    });
   }
   render() {
     return (
       <div>
         <h1>Restaurant List</h1>
-        {
-          this.state.list?
+        {this.state.list ? (
           <div>
-            {
-              this.state.list.map((item,i)=>
-                <div className='list-wrapper'>
-                  <span>{item.name}</span>
-                  <span>{item.email}</span>
-                  <span>{item.address}</span>
-                  <span>{item.rating}</span>
-                </div>
-
-              )
-            }
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Rating</th>
+                  <th>Location</th>
+                  <th>Email</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.list.map((item, i) => (
+                  <tr>
+                    <td>{i+1}</td>
+                    <td>{item.name}</td>
+                    <td>{item.rating}</td>
+                    <td>{item.address}</td>
+                    <td>{item.email}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
           </div>
-          :<p>Please Wait...</p>
-        }
+        ) : (
+          <p>Please Wait...</p>
+        )}
       </div>
-    )
+    );
   }
 }
