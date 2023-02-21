d571c1a6e2515d7e6abf0ee544106e121086ea8a
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-21 18:24:45 +0530

    Integrating list api to the app

--- restaurant-app/src/components/RestaurantList.js
+++ restaurant-app/src/components/RestaurantList.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react'
 
 export default class RestaurantList extends Component {
+  constructor(){
+    super();
+    this.state={
+      list : null,
+    }
+  }
+  componentDidMount(){
+    fetch("http://localhost:3000/restaurant").then((response)=>{
+      response.json().then((result)=>{
+        //console.warn(result);
+        this.setState({list:result});
+      })
+    })
+  }
   render() {
     return (
-      <div>RestaurantList</div>
+      <div>
+        <h1>Restaurant List</h1>
+        {
+          this.state.list?
+          <div>
+            {
+              this.state.list.map((item,i)=>
+                <div className='list-wrapper'>
+                  <span>{item.name}</span>
+                  <span>{item.email}</span>
+                  <span>{item.address}</span>
+                  <span>{item.rating}</span>
+                </div>
+
+              )
+            }
+          </div>
+          :<p>Please Wait...</p>
+        }
+      </div>
     )
   }
 }
