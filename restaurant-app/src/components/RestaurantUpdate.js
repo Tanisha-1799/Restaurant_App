d6bad6159bc072b6a4d54433c0d2b1c0d6097f2a
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-23 18:49:37 +0530

    Integrating the update api to the app

--- restaurant-app/src/components/RestaurantUpdate.js
+++ restaurant-app/src/components/RestaurantUpdate.js
@@ -2,30 +2,62 @@ import React, { Component } from 'react';
 import withRouter from './withRouter';
 
 class RestaurantUpdate extends Component {
+
+  constructor(){
+    super();
+    this.state={
+      name:null,
+      email:null,
+      address:null,
+      rating:null,
+      id:null
+    }
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/restaurant/"+this.props.params.id).then((res)=>{
+      res.json().then((result)=>{
+        this.setState({
+          name:result.name,
+          email:result.email,
+          address:result.address,
+          rating:result.rating,
+          id:result.id
+        })
+
+      })
+    })
+  }
   render() {
-    //console.log('Props:', this.props)
+    console.log(this.props.params.id);
     return (
       <div>
         <h1>Restaurant Update component</h1>
-        {/**<div>Id: {this.props.params.id}</div> */}
+       
         <div>
           <input onChange={(event)=>{this.setState({name:event.target.value})}}
            type="text" 
            placeholder="Restaurant Name"
-            
+           value={this.state.name} 
            /><br></br>
            <br></br>
            <input onChange={(event)=>{this.setState({rating:event.target.value})}}
            type="text" 
-           placeholder="Restaurant Rating"/><br></br>
+           placeholder="Restaurant Rating"
+           value={this.state.rating} 
+           /><br></br>
            <br></br>
            <input onChange={(event)=>{this.setState({address:event.target.value})}}
            type="text" 
-           placeholder="Restaurant Location"/><br></br>
+           placeholder="Restaurant Location"
+           value={this.state.address} 
+           /><br></br>
            <br></br>
            <input onChange={(event)=>{this.setState({email:event.target.value})}}
            type="text" 
-           placeholder="Restaurant Email"/><br></br>
+           placeholder="Restaurant Email"
+           value={this.state.email} 
+           /><br></br>
            <br></br>
            <button onClick={()=>{this.update()}}>Update Restaurant</button>
         </div>
@@ -35,5 +67,5 @@ class RestaurantUpdate extends Component {
   }
 };
 
-//export default withRouter(RestaurantUpdate);
-export default RestaurantUpdate;
+export default withRouter(RestaurantUpdate);
+//export default RestaurantUpdate;
