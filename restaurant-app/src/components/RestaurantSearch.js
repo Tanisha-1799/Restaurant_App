ef000bf9abc44b09b27c3bfba7b59b748768a5c7
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-25 19:50:45 +0530

    Integrating the search api

--- restaurant-app/src/components/RestaurantSearch.js
+++ restaurant-app/src/components/RestaurantSearch.js
@@ -9,7 +9,12 @@ export default class RestaurantSearch extends Component {
     }
   }
   search(key){
-    console.warn(key);
+    //console.warn(key);
+    fetch("http://localhost:3000/restaurant?q="+key).then((result)=>{
+      result.json().then((res)=>{
+       this.setState({searchData:res})
+      })
+    })
   }
 
 
@@ -18,6 +23,19 @@ export default class RestaurantSearch extends Component {
       <div>
         <h1>Restaurant Search</h1>
         <input type="text"onChange={(event)=>this.search(event.target.value)}/>
+        <div>
+          {
+            this.state.searchData?
+            <div>
+              {this.state.searchData.map((item)=>
+                <div>
+                  {item.name}
+                </div>
+                )}
+            </div>
+            :"No result Found"
+          }
+        </div>
 
       </div>
     )
