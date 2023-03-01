bb5d031dbd43a577871b1fcf8aee5c5185bad602
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-03-01 18:12:25 +0530

    Adding header

--- restaurant-app/src/components/Login.js
+++ restaurant-app/src/components/Login.js
@@ -18,6 +18,7 @@ export default class Login extends Component {
     fetch("http://localhost:3000/login?q="+this.state.name).then((result)=>{
       result.json().then((res)=>{
        if(res.length>0){
+        localStorage.setItem("login",JSON.stringify(res));
         this.setState({loginCorrect:true})
 
        }else{
